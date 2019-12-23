d25e69caeb4fd8d1fe8fa3665eef5cd903cb8e8f
Amar <prashanth@Prashanths-MacBook-Pro-2.local>
2019-12-23 08:00:13 +0530

    Added required changes

--- src/app/firstadmin/search-contact/search-contact.component.ts
+++ src/app/firstadmin/search-contact/search-contact.component.ts
@@ -1,17 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import {Router} from '@angular/router';
+
+
+const states = [ 'Deanaa Smith', 'Anne Smith', 'Renee smith', 'Greg smithe', 'Patracia Smithers'];
 
 @Component({
   selector: 'app-search-contact',
   templateUrl: './search-contact.component.html',
   styleUrls: ['./search-contact.component.css']
 })
+
+
 export class SearchContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  public model: string;
+  public selectedProspect: boolean;
+
 
   ngOnInit() {
   }
+  search = (text$: Observable<string>) =>
+  text$.pipe(
+    debounceTime(200),
+    distinctUntilChanged(),
+    map(term => term.length < 2 ? []
+      : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+  )
+
   goBack(){
     
+  }
+
+  selectContact (event) {
+    //this.router.navigateByUrl('/directseller/contactInformation')
+  event.preventDefault()
+  this.selectedProspect = true
   }
 }
