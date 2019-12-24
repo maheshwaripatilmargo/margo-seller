9182000f8dd8830185830b202332438ef1809ad8
maheswaripatilmago <maheshwari.p@aditatechnologies>
2019-12-24 16:44:35 +0530

    t1

--- src/app/firstadmin/getsocial/customfbpost/customfbpost.component.ts
+++ src/app/firstadmin/getsocial/customfbpost/customfbpost.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FbService } from 'src/app/services/fbservice/fb.service';
 
 @Component({
   selector: 'app-customfbpost',
@@ -6,10 +8,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customfbpost.component.css']
 })
 export class CustomfbpostComponent implements OnInit {
+  post_name;
+  category_id;
+  post_id;
+  post_Url;
+  post_screenshot;
+  status=true;
+  postValue:any;
+  msgVal:any;
+ 
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute,private fbservice: FbService) { }
 
-  ngOnInit() {
+   ngOnInit() {
+     let self = this;
+  this.activatedRoute.queryParams
+    .subscribe(params => {
+        self.post_id = params.post_id;
+      });
+      this.msgSnipet(self.post_id);
+
+    //get item details using id
+    if (self.post_id) {
+      console.log("kkkkk",self.post_id);
+      this.fbservice.getCustomData(this.post_id).subscribe(response => {
+        self.postValue = response.data;
+        // console.log("qq",response);
+        if (response.status) {
+         this.status = false;
+          self.postValue = response.data;
+          console.log("postValue",self.postValue);
+         self.dataBinding();
+        } else {
+
+        }
+      });
+    }
+  }
+  msgSnipet(post_id){
+    console.log("msgsnipet",post_id);
+    let self = this;
+    this.fbservice.getCustomMessage(post_id).subscribe(response => {
+      self.msgVal = response;
+      console.log("msgVal",self.msgVal);
+      if (response.status) {
+       this.status = false;
+       self.msgVal = response.data;
+      //  self.dataBinding();
+      } else {
+
+      }
+    });
+
+  }
+  
+  dataBinding() {
+    console.log("zz")
+    this.post_name = this.postValue.post_name;
+    this.category_id = this.postValue.category_id;
+    this. post_id = this.postValue.post_id;
+    console.log(this.post_id);
+    this.post_Url = this.postValue.post_Url;
+    
+    
+    this.post_screenshot = this.postValue.post_screenshot;
+    console.log(this.post_screenshot);
   }
 
 }
--- src/app/firstadmin/getsocial/fbcampaignname/fbcampaignname.component.ts
+++ src/app/firstadmin/getsocial/fbcampaignname/fbcampaignname.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FbService } from 'src/app/services/fbservice/fb.service';
 
 @Component({
   selector: 'app-fbcampaignname',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fbcampaignname.component.css']
 })
 export class FbcampaignnameComponent implements OnInit {
-
-  constructor() { }
-
+  
+  
+  constructor(private ActivatedRoute:ActivatedRoute, private FbService:FbService ) { }
+data=[];
+msg=[];
+img=[];
+status=true;
   ngOnInit() {
+    let self=this;
+    this.ActivatedRoute.queryParams.subscribe(params => {
+      self.data = params.post_id.splice(',');
+      
+        console.log("lll",self.data);
+        
+        
+      });
+      self.campaignMsgSnipet(this.data);
+    this.getCampaignImages(this.data);
+    
+  }
+ campaignMsgSnipet(data){
+    let self=this;
+    console.log("qqq",this.data);
+    this.FbService.getCustomMessage(data).subscribe(response => {
+      // self.msg = response;
+      // console.log("msg",self.msg);
+      if (response.status) {
+       this.status = false;
+       self.msg = response.data;
+       //self.dataBinding();
+      } else {
+      }
+    });
+
   }
 
+  getCampaignImages(data){
+    let self=this;
+    console.log("rq",this.data);
+    this.FbService.getCustomimages(data).subscribe(response => {
+      self.img = response;
+      console.log("img",self.img);
+      if (response.status) {
+       this.status = false;
+       self.img = response.data;
+       //self.dataBinding();
+      } else {
+      }
+    });
+
+  }
 }
+
+ 
